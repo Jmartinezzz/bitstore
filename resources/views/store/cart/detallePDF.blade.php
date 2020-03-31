3136f2bfa88d061baa72e1424c9d1e59e743e172
Jorge M <m.jorge1slv@yahoo.es>
2020-03-31 14:56:10 -0600

    se agregaron los archivos faltantes

---
+++ resources/views/store/cart/detallePDF.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+</head>
+<body>  
+<!-- contenido -->
+<div class="container">
+    <div class="row text-center mb-3">
+        <div class="col-md-6">
+            <img src="img/logos/logo1.jpg" alt="logo bitstore" style="width: 40%">
+        </div>
+    </div>
+    <div class="row text-center mb-3">
+        <div class="card-header bg-warning text-white mb-2">
+            <h4>Detalle de pedido</h4>
+            <h5>Cliente: {{ $datosUsuario->name ." ". $datosUsuario->lastName}}</h4>
+            <h6>Orden: {{ $productos->id }}</h6>
+        </div>
+    </div>
+    <table class="table">
+        <tr>
+            <th>Producto</th>
+            <th>Precio</th>
+            <th>Cantidad</th>
+            <th>Sub total</th>
+        </tr>
+        <tbody>
+            @foreach ($productos->products as $prod)  
+               <tr>
+                <td>{{ $prod->productName }}</td>
+                <td>${{ $prod->salePrice }}</td>
+                <td>{{ $prod->pivot->quantity }}</td>
+                <td>${{ $prod->pivot->subTotal }}</td>  
+               </tr>                      
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="2" class="text-left">
+                    Firma.<img style="width: 200px" src="img/firmas/{{ $datosUsuario->id }}.png" alt="">
+                </th>
+                <th colspan="2" class="text-right">Total: ${{ $productos->total }}</th>
+            </tr>
+        </tfoot>
+    </table>       
+    <hr class="bg-dark">
+   
+</div> <!-- contenido -->
+
+    
+</body>
+</html>
---
+++ resources/views/store/cart/history.blade.php
@@ -0,0 +1,98 @@
+@extends('store.partials.principal')
+@section('title',"Historial de compras")
+@section('activeUser',"active")
+@section('content')
+<!-- contenido -->
+<div class="container">
+    <div class="row justify-content-center my-5">
+        <div class="col-md-10 ">
+            <div class="card text-center">
+                <div class="card-header bg-warning text-white">
+                    <h4>Historial de pedidos</h4>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12">                            
+                        </div>
+                        <div class="col-12">
+                            <div class="tab-content" id="nav-tabContent">
+                                <!-- nav 1 (carrito) -->
+                                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-carrito">
+                                @if (count($productos) < 1 )
+                                    <div class="row my-5 justify-content-center"> 
+                                        <h3>¡Aún no tienes compras!</h3>
+                                        <div class="col-12">
+                                            <a href="{{ route('products.index') }}">Ver productos</a>
+                                        </div>
+                                    </div>   
+                                @endif                                
+                                @for ($i = 0; $i < count($productos) ; $i++)
+                                <div class="row justify-content-start">
+                                    <div class="col-md-3">
+                                        <label class="h5">Orden: {{ $productos[$i]->id }}</label>
+                                        <form action="{{ route('cart.pdf') }}" method="post">
+                                            @csrf 
+                                            <input type="hidden" name="id" value="{{ $productos[$i]->id}}">                                           
+                                            <button class="btn btn-outline-warning btn-sm">Exportar detalle <i class="fas fa-file-pdf fa-2x"></i></button>
+                                        </form>
+                                    </div>
+                                 </div>
+                                   @foreach ($productos[$i]->products as $prod)
+                                        <div class="row my-3">                          
+                                            <div class="col-5">
+                                            @isset ($prod->images)
+                                                @foreach ($prod->images as $imge)
+                                                    <img src="{{ asset('img/productos/' . $imge->img) }}" class="img-fluid card-img-top" style="width: 220px; height: 200px">
+                                                @break                                        
+                                                @endforeach                                                
+                                            @endisset
+                                            </div>
+                                        <div class="col-7 text-left">
+                                            <div class="form-group">
+                                                <label class="h3">{{ $prod->productName }}</label>                                              
+                                            </div>
+                                            <div class="form-group">                                                   
+                                                <label class="h5">precio: ${{ $prod->salePrice }}</label>
+                                                <input class="precio" type="hidden" value="{{ $prod->salePrice }}">
+                                            </div>
+                                            <div class="form-group">
+                                                 <label class="h5">Cantidad: {{ $prod->pivot->quantity }}</label>                                                
+                                            </div>
+                                             <div class="form-group">
+                                                <label class="h5 subTotal">Sub total: ${{ $prod->pivot->subTotal }}</label>                                                
+                                            </div>
+                                        </div>  
+                                        </div>
+                                        <hr class="bg-warning">                                                           
+                                    @endforeach
+                                    <div class="row justify-content-end">
+                                        <div class="col-md-6">
+                                            <label class="h5">Total: ${{ $productos[$i]->total }}</label>
+                                        </div>
+                                     </div>
+
+                                    <hr class="bg-dark"> 
+                                @endfor                                  
+                                </div><!-- nav 1 (carrito) -->                                             
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer text-muted bg-warning">
+                    <!-- text -->
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- contenido -->
+@endsection
+@section('scriptsFooter')
+
+<script>   
+   
+    $(function(){
+            
+    })
+   
+</script>
+@endsection
